3198f3a0a944d1c028f10f7a6271632749e6083b
Lisa BELLALI <lisa.bellali92@gmail.com>
2025-03-03 00:42:22 +0100

    router avancé

--- server/src/router.ts
+++ server/src/router.ts
@@ -4,9 +4,7 @@ const router = express.Router();
 
 /* ************************************************************************* */
 // Define Your API Routes Here
-import programActions from "./modules/program/programActions";
-
-router.get("/api/Programs", programActions.browse);
+/* ************************************************************************* */
 
 // Define item-related routes
 import itemActions from "./modules/item/itemActions";
@@ -15,10 +13,20 @@ router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
+// Define program-related routes
+import programActions from "./modules/program/programActions";
+
+router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+
+router.get("/api/categories", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
+
 /* ************************************************************************* */
 
 // Declaration of a "Welcome" route
 
+import categoryActions from "./modules/category/categoryActions";
 import sayActions from "./modules/say/sayActions";
 
 router.get("/", sayActions.sayWelcome);
